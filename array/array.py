b1426c74dd0fc255e43bd0241ca79fb32e1519f9
MAKOSCAFEE <barnabasmakonda@gmail.com>
2016-11-13 23:19:20 +0300

    new array

---
+++ array/array.py
@@ -0,0 +1,11 @@
+
+
+class Array(array):
+    def __init__(array=[]):
+        self.array = array
+
+    def push(item):
+        return self.array.append(item)
+
+    def pop():
+        return self.array.pop()
